7a80a7751344f41dcb8f895472670251eb78f341
alexdev2001 <aleximani201@gmail.com>
2024-09-20 20:24:50 +0200

    constructed the register user page

--- src/app/page.js
+++ src/app/page.js
@@ -76,7 +76,7 @@ export default function Home() {
                       <path strokeLinecap="round" strokeLinejoin="round"
                         d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
                   </svg>
-                  <input type="text" className="grow"
+                  <input type="text" className="grow p-2"
                       placeholder="Enter username" required
                        value={username}
                        onChange={(e) => setUsername(e.target.value)}
@@ -91,7 +91,7 @@ export default function Home() {
                         <path strokeLinecap="round" strokeLinejoin="round"
                             d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
                     </svg>
-                    <input type="password" className="grow"
+                    <input type="password" className="grow p-2"
                           placeholder="Enter password" required
                           value={password}
                           onChange={(e) => setPassword(e.target.value)}
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,8 +1,36 @@
 
 export default function Page() {
     return(
-        <h1 className="text-black">
-            create user
-        </h1>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className={'flex flex-col gap-8 row-start-2 items-center sm:items-start'}>
+                <div className="text-center">
+                    <h1 className="font-sans text-black text-4xl mb-1 text-center">
+                        Create account
+                    </h1>
+                </div>
+                <form className="text-center">
+                    <label className={`input input-bordered flex text-center gap-2 pt-2 text-black`}>
+                        <input type="email" placeholder="email" className="p-2 grow"></input>
+                    </label>
+                    
+                    <label className={`input input-bordered flex text-center gap-2 pt-2 text-black`}>
+                        <input type="username" placeholder="username" className="p-2 grow "></input>
+                    </label>
+                    <label className={`input input-bordered flex text-center gap-2 pt-2 text-black`}>
+                        <input type="password" placeholder="password" className="p-2 grow"></input>
+                    </label>
+                    <label className={`input input-bordered flex text-center gap-2 pt-2 text-black`}>
+                        <input type="password" placeholder="verify password" className="p-2 grow"></input>
+                    </label>
+                    
+                    <button className="btn btn-primary w-full
+               hover:text-white border-solid hover:bg-red-600 p-3 mt-3 rounded-md mb-3 bg-white border-2 border-red-500 
+               text-md text-black ">
+                        Register
+                    </button>
+                </form>
+            </main>
+           
+        </div>
     )
 }
