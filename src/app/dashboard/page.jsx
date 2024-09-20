9d20abaa53efba4355e2953a4d7e0d697d8b9af7
alexdev2001 <aleximani201@gmail.com>
2024-09-20 12:20:07 +0200

    fixed routing

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 
-export default function dashboard() {
+export default function Page() {
     return(
-        <h1>
+        <h1 className="text-black">
             You are in the dashboard
         </h1>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -72,7 +72,7 @@ export default function Home() {
                   className={`input input-bordered flex items-center gap-2 text-black `}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                   strokeWidth="1.5"
-                  stroke="currentColor" className="w-6 h-8 text-center text-red-500">
+                  stroke="currentColor" className="w-5 h-6 text-center text-red-500">
                       <path strokeLinecap="round" strokeLinejoin="round"
                         d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
                   </svg>
@@ -87,7 +87,7 @@ export default function Home() {
                     className={`input input-bordered flex text-center gap-2 pt-2 text-black`}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         strokeWidth="1.5"
-                        stroke="currentColor" className="w-6 h-8 text-red-500">
+                        stroke="currentColor" className="w-5 h-6 text-red-500">
                         <path strokeLinecap="round" strokeLinejoin="round"
                             d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
                     </svg>
@@ -99,14 +99,17 @@ export default function Home() {
                 </label>
               </div>
               <button  type="submit" onClick={handleLogin} className="btn btn-primary w-full
-               text-white border-solid bg-red-600 p-3 mt-3 rounded-md mb-3 hover:bg-white border-2 border-red-500 
-               text-md hover:text-black             
+               hover:text-white border-solid hover:bg-red-600 p-3 mt-3 rounded-md mb-3 bg-white border-2 border-red-500 
+               text-md text-black             
                ">
                 Login 
               </button>
               {errorMessage && <p style={{color: "red"}} className="text-center">{errorMessage}</p>}
               <p>
-                  <a href="" className="text-primary text-red-500 p-4 text-sm mx-20 text-center mb-5 hover:underline">
+                  <a href=""  onClick={(e) => {
+                    e.preventDefault();
+                    router.push('/register');
+                  }} className="text-primary text-red-500 p-4 text-sm mx-20 text-center mb-5 hover:underline">
                       Create account
                   </a>
               </p>
